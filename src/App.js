f3c7a0c73b66dc3e417436cbc6895f3201e80e46
Arslan Khan <alkabeer.info.co@gmail.com>
2022-07-13 10:39:45 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import HomePage from './pages/Home'
+
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
